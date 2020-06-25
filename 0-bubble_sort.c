24e4769b405cd44c5bc891fcd6868eb46d438cda
pasignature <pasignature@gmail.com>
2020-06-25 18:28:59 +0000

    updates

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,31 +1,34 @@
 #include "sort.h"
 
 /**
- * bubble_sort - sorts array with bs algorith
- * @size: size of hash table
- * @array: array of ints
- * Return: void
+ * bubble_sort - C function that uses uses the Bubble sort method in an array
+ * @array: array of integers to sort
+ * @size: the size of the array to be sorted
+ * Return: sorted array
  */
 
 void bubble_sort(int *array, size_t size)
 {
-  size_t checker = size;
-  size_t i, holder = 0;
+  int holder = 1;
+  unsigned int i;
 
-  if (array == NULL || size == 0)
+  if (!array || size < 2)
     return;
-  while (checker > 0)
+
+  while (holder)
     {
+      holder = 0;
       for (i = 0; i < size - 1; i++)
 	{
 	  if (array[i] > array[i + 1])
 	    {
-	      holder = array[i + 1];
+	      int next = array[i + 1];
+
 	      array[i + 1] = array[i];
-	      array[i] = holder;
+	      array[i] = next;
+	      holder = 1;
 	      print_array(array, size);
 	    }
 	}
-      checker--;
     }
 }
